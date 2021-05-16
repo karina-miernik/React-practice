213d26fbe278b823634ef4f6c2ed1d182958ef76
Karina Miernik <karina.miernik@gmail.com>
2021-05-16 13:21:20 +0200

    links added

--- src/components/Search.js
+++ src/components/Search.js
@@ -24,6 +24,13 @@ const Search = () => {
   const renderedResults = result.map((result) => {
     return (
       <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a 
+          className="ui button" 
+          href={`https://en.wikipedia.org?curid=${result.pageid}`}>
+            Go
+            </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
           <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
