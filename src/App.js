d964bf01dfa0bed21078dfca188e580e7d1c42f3
Karina Miernik <karina.miernik@gmail.com>
2021-07-09 13:12:02 +0200

    dropdown added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Accordion from './components/Accordion'
 import Search from './components/Search'
+import Dropdown from './components/Dropdown'
 const items = [
   {
     title: 'What is React',
@@ -15,11 +16,23 @@ const items = [
     content: 'You use React by creating components',
   },
 ]
-
+const options = [
+  {
+    label: 'The colour of Red',
+    value: 'Red'
+  },  {
+    label: 'The colour of Blue',
+    value: 'Blue'
+  },  {
+    label: 'The colour of Green',
+    value: 'Green'
+  },
+ 
+]
 export default () => {
   return (
     <div>
-      <Search />
+      <Dropdown options={options}/>
       {/* <Accordion items={items} /> */}
     </div>
   )
