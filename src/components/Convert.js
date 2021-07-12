2c3defa9f1667f94e35699d3c0d8366d686e5429
Karina Miernik <karina.miernik@gmail.com>
2021-07-12 20:10:42 +0200

    added axios to Convert

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react'
-
+import axios from 'axios'
 const Convert = ({language, text}) => {
   useEffect(() => {
-    console.log('New language or text')
+    
   }, [language, text])
 
   return (
