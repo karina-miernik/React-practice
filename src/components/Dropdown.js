7316c3cb1a8e57d281703c7334c8be9b81c822a7
Karina Miernik <karina.miernik@gmail.com>
2021-07-12 19:08:44 +0200

    eventListener cleanup

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,12 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 
 const Dropdown = ({options, selected, onSelectedChange}) =>{
   const [open, setOpen] = useState(false)
+  const ref = useRef()
    useEffect (() => {
-    document.body.addEventListener('click', () => {
-    setOpen(false)
-    })
+     const onBodyClick = (event) => {
+      if (ref.current.contains(event.target)){
+      return
+    }
+      setOpen(false)
+    }
+    document.body.addEventListener('click',  onBodyClick)
+    return () => document.body.removeEventListener('click', onBodyClick)
    }, [])
 
   const renderedOptions = options.map((option)=> {
@@ -22,7 +28,7 @@ const Dropdown = ({options, selected, onSelectedChange}) =>{
     )
   })
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a colour</label>
         <div 
