0c3df914335b44e325a8b00d7136fd035ae1d537
Karina Miernik <karina.miernik@gmail.com>
2021-07-30 13:14:34 +0200

    navigation with router implemented

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Accordion from './components/Accordion'
 import Search from './components/Search'
 import Dropdown from './components/Dropdown'
 import Translate from './components/Translate'
+import { BrowserRouter, Switch, Route, Link, NavLink } from 'react-router-dom';
 const items = [
   {
     title: 'What is React',
@@ -31,16 +32,43 @@ const options = [
   },
  
 ]
-export default () => {
-  // const [selected, setSelected] = useState(options[0])
-  return (
-    <div>
-      <Translate/>
-      {/* <Dropdown 
-      selected={selected} 
-      onSelectedChange = {setSelected}
-      options={options}/> */}
 
-      </div>
-  )
+export default () => {
+  const [selected, setSelected] = useState(options[0])
+  return(
+  <BrowserRouter>
+      <div>
+        <header>
+        <div className="ui secondary pointing menu">
+            <NavLink  activeClassName="active-nav" to="/" className="item">Accordion</NavLink>
+            <NavLink activeClassName="active-nav" to="/list" className="item">Search</NavLink>
+            <NavLink activeClassName="active-nav" to="/dropdown" className="item">Dropdown</NavLink>
+            <NavLink activeClassName="active-nav" to="/translate" className="item">Translate</NavLink>
+          </div>
+        </header>
+        </div>
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Accordion items={items}/>
+            </Route>
+            <Route path="/list">
+              <Search/>
+            </Route>
+            <Route path="/dropdown">
+             <Dropdown
+              label="select a colour"
+              options={options}
+              selected={selected}
+              onSelectedChange={setSelected}
+            />
+            </Route>
+            <Route path="/translate">
+              <Translate/>
+            </Route>
+            </Switch>
+            </div>
+    </BrowserRouter>
+  ) 
+
 }
